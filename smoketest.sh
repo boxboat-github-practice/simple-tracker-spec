f7410ea2a2ad52183a537edeead45f30d6e36929
Daniel Koch <dkoch84@gmail.com>
2023-08-11 16:11:47 -0500

    Create smoketest.sh
    Signed-off-by: Daniel Koch <dkoch84@gmail.com>

---
+++ smoketest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+collection="sql-test-collection.json"
+environment="Local.postman_environment.json"
+repo="https://api.github.com/repos/boxboat-github-practice/simple-tracker-spec/contents"
+
+HALP() {
+  printf '\nsmoketest.sh <test type>'
+  printf '\nwhere <test type> is one of [employees, clients, contracts, history]\n'
+}
+
+if [ $1 -z ]; then
+  HALP
+fi
+
+newman run -k -n 1 -r cli --folder $1 -e \
+  <(curl -s ${repo}/${environment} | jq  -r .content | base64 -d) \
+  <(curl -s ${repo}/${collection} | jq  -r .content | base64 -d) \
+
+exit 0
