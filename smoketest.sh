9e8b36d67f2d171bee3cbee4d8579d81fcd4b641
Daniel Koch <dkoch84@gmail.com>
2023-08-22 16:29:22 -0500

    i read the manpage for `test`

--- smoketest.sh
+++ smoketest.sh
@@ -7,14 +7,16 @@ repo="https://api.github.com/repos/boxboat-github-practice/simple-tracker-spec/c
 HALP() {
   printf '\nsmoketest.sh <test type>'
   printf '\nwhere <test type> is one of [employees, clients, contracts, history]\n'
+  exit 1
 }
 
-if [ "$1" -z ]; then
+if [ -z "$1" ]
+then
   HALP
+else
+  newman run -k -n 1 -r cli --folder $1 -e \
+    <(curl -s ${repo}/${environment} | jq  -r .content | base64 -d) \
+    <(curl -s ${repo}/${collection} | jq  -r .content | base64 -d) 
 fi
-
-newman run -k -n 1 -r cli --folder $1 -e \
-  <(curl -s ${repo}/${environment} | jq  -r .content | base64 -d) \
-  <(curl -s ${repo}/${collection} | jq  -r .content | base64 -d) \
 
 exit 0
